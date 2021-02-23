0779d6113577904e254c9a9b1bbb67953f48e725
lyrio94 <alexandrelyrio94@hotmail.com>
2021-02-22 21:45:34 -0300

    commit segundo

---
+++ app/src/main/java/projeto/br/injecaodependencias/models.kt
@@ -0,0 +1,11 @@
+package projeto.br.injecaodependencias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class models : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_models)
+    }
+}
